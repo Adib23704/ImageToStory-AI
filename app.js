95f0e7fe71b65445570844bcd3875b7f8b86ad2e
Adib23704 <adib23704@gmail.com>
2024-03-12 21:58:16 +0600

    Type Fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const hgInference = new HfInference(process.env.HUGGINGFACE_TOKEN);
 
 async function main() {
 	const output = await hgInference.imageToText({
-		data: readFileSync('./img/img.png'),
+		data: fs.readFileSync('./img/img.png'),
 		model: 'Salesforce/blip-image-captioning-base',
 	});
 	console.log('output :>> ', output);
